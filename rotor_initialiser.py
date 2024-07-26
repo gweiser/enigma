e5e8dfb3dce64cee490e502ade6343bfd259b514
gregolator <gweiser54@icloud.com>
2024-07-26 16:33:32 +0200

    Added multiple rotors functionality

--- rotor_initialiser.py
+++ rotor_initialiser.py
@@ -1,7 +1,9 @@
 def initialiser(start_num):
     rotor = []
+    # Fill rotor with digits from start_num to 26
     for i in range(27-int(start_num)):
         rotor.append(i+start_num)
+    # Fill up rest of rotor
     if len(rotor) < 26:
         for i in range(start_num-1):
             rotor.append(i+1)
@@ -12,9 +14,16 @@ def initialiser(start_num):
 
 
 def main():
-    start_num = int(input("Starting number: "))
-    rotor = initialiser(start_num)
-    print(rotor)
+    rotor_num = int(input("Number of rotors: "))
+    rotors = {}
+    for rotor in range(rotor_num):
+        start_num = int(input("Starting number: "))
+        rotors[rotor] = initialiser(start_num)
+
+    # start_num = int(input("Starting number: "))
+    # rotor = initialiser(start_num)
+    for i in rotors:
+        print(rotors[i])
 
 
 main()
