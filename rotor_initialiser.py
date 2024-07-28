9a5c936f03148dbc9e2686d1e1c9bdfca9eeca74
gregolator <gweiser54@icloud.com>
2024-07-28 18:17:47 +0200

    Added new file for hard-coded rotors and reflector, like in the original enigma, and removed old counterparts dictionary

--- rotor_initialiser.py
+++ rotor_initialiser.py
@@ -1,15 +1,5 @@
 rotor_base = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
 
-# Counterparts for each of the five rotors (originals were also hard-coded)
-counterparts = {
-    1: [2, 16, 26, 21, 12, 5, 13, 18, 17, 25, 20, 23, 10, 1, 7, 4, 22, 11, 24, 19, 8, 14, 6, 9, 3, 15],
-    2: [1, 6, 7, 26, 25, 2, 23, 12, 15, 8, 4, 17, 11, 14, 24, 20, 13, 9, 10, 16, 3, 22, 21, 5, 19, 18],
-    3: [20, 21, 7, 26, 24, 13, 6, 11, 5, 25, 15, 22, 3, 4, 16, 14, 17, 12, 10, 1, 19, 9, 8, 23, 18, 2],
-    4: [25, 7, 26, 17, 1, 8, 3, 6, 22, 24, 19, 4, 20, 2, 11, 23, 10, 16, 15, 18, 9, 14, 13, 5, 12, 21],
-    5: [17, 11, 8, 14, 23, 24, 12, 3, 10, 6, 5, 2, 19, 13, 25, 21, 20, 1, 9, 16, 4, 15, 18, 7, 26, 22]
-}
-
-
 def initialiser(rotor_num, start_num):
     """Initialise a rotor and its counterpart"""
     rotor = []
@@ -57,17 +47,6 @@ def main():
     r2 = initialiser(r2_rotor, r2_start)
     r3 = initialiser(r3_rotor, r3_start)
 
-    # print(r1["rotor"])
-    # print(r1["counterpart"])
-    # print(r2["rotor"])
-    # print(r2["counterpart"])
-    # print(r3["rotor"])
-    # print(r3["counterpart"])
-
-    index = r1["rotor"].index(start_num)
-    r1_counterpart = r1["counterpart"][index]
-    r2_counterpart = r2["counterpart"][index]
-    r3_counterpart = r3["counterpart"][index]
 
 
 
