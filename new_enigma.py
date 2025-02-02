1ea130f10f3e5aeb7eebfdf91dd9eaf6d162335e
gregolator <gweiser54@icloud.com>
2025-02-02 22:51:54 +0100

    Worked on rotor representation in code, moved to list of dicts, and pseudocoded proper logic for rotation of rotors relative to turnover position

--- new_enigma.py
+++ new_enigma.py
@@ -3,31 +3,50 @@ alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N'
 reflector = ['E', 'J', 'M', 'Z', 'A', 'L', 'Y', 'X', 'V', 'B', 'W', 'F', 'C', 'R', 'Q', 'U', 'O', 'N', 'T', 'S', 'P', 'I', 'K', 'H', 'G', 'D']
 reflector_b = ['Y', 'R', 'U', 'H', 'Q', 'S', 'L', 'D', 'P', 'X', 'N', 'G', 'O', 'K', 'M', 'I', 'E', 'B', 'F', 'Z', 'C', 'W', 'V', 'J', 'A', 'T']
 
+# all_rotors = [
+#     ['E', 'K', 'M', 'F', 'L', 'G', 'D', 'Q', 'V', 'Z', 'N', 'T', 'O', 'W', 'Y', 'H', 'X', 'U', 'S', 'P', 'A', 'I', 'B', 'R', 'C', 'J'], # Turnover: Q
+#     ['A', 'J', 'D', 'K', 'S', 'I', 'R', 'U', 'X', 'B', 'L', 'H', 'W', 'T', 'M', 'C', 'Q', 'G', 'Z', 'N', 'P', 'Y', 'F', 'V', 'O', 'E'], # Turnover: E
+#     ['B', 'D', 'F', 'H', 'J', 'L', 'C', 'P', 'R', 'T', 'X', 'V', 'Z', 'N', 'Y', 'E', 'I', 'W', 'G', 'A', 'K', 'M', 'U', 'S', 'Q', 'O'], # Turnover: V
+#     ['E', 'S', 'O', 'V', 'P', 'Z', 'J', 'A', 'Y', 'Q', 'U', 'I', 'R', 'H', 'X', 'L', 'N', 'F', 'T', 'G', 'K', 'D', 'C', 'M', 'W', 'B'], # Turnover: J
+#     ['V', 'Z', 'B', 'R', 'G', 'I', 'T', 'Y', 'U', 'P', 'S', 'D', 'N', 'H', 'L', 'X', 'A', 'W', 'M', 'J', 'Q', 'O', 'F', 'E', 'C', 'K']  # Turnover: Z
+# ]
+
+# all_rotors = {
+#     "rotor_one": ['E', 'K', 'M', 'F', 'L', 'G', 'D', 'Q', 'V', 'Z', 'N', 'T', 'O', 'W', 'Y', 'H', 'X', 'U', 'S', 'P', 'A', 'I', 'B', 'R', 'C', 'J'],
+#     "rotor_two": ['A', 'J', 'D', 'K', 'S', 'I', 'R', 'U', 'X', 'B', 'L', 'H', 'W', 'T', 'M', 'C', 'Q', 'G', 'Z', 'N', 'P', 'Y', 'F', 'V', 'O', 'E'],
+#     "rotor_three": ['B', 'D', 'F', 'H', 'J', 'L', 'C', 'P', 'R', 'T', 'X', 'V', 'Z', 'N', 'Y', 'E', 'I', 'W', 'G', 'A', 'K', 'M', 'U', 'S', 'Q', 'O'],
+#     "rotor_four": ['E', 'S', 'O', 'V', 'P', 'Z', 'J', 'A', 'Y', 'Q', 'U', 'I', 'R', 'H', 'X', 'L', 'N', 'F', 'T', 'G', 'K', 'D', 'C', 'M', 'W', 'B'],
+#     "rotor_five": ['V', 'Z', 'B', 'R', 'G', 'I', 'T', 'Y', 'U', 'P', 'S', 'D', 'N', 'H', 'L', 'X', 'A', 'W', 'M', 'J', 'Q', 'O', 'F', 'E', 'C', 'K']
+# }
+
 all_rotors = [
-    ['E', 'K', 'M', 'F', 'L', 'G', 'D', 'Q', 'V', 'Z', 'N', 'T', 'O', 'W', 'Y', 'H', 'X', 'U', 'S', 'P', 'A', 'I', 'B', 'R', 'C', 'J'], # Turnover: Q
-    ['A', 'J', 'D', 'K', 'S', 'I', 'R', 'U', 'X', 'B', 'L', 'H', 'W', 'T', 'M', 'C', 'Q', 'G', 'Z', 'N', 'P', 'Y', 'F', 'V', 'O', 'E'], # Turnover: E
-    ['B', 'D', 'F', 'H', 'J', 'L', 'C', 'P', 'R', 'T', 'X', 'V', 'Z', 'N', 'Y', 'E', 'I', 'W', 'G', 'A', 'K', 'M', 'U', 'S', 'Q', 'O'], # Turnover: V
-    ['E', 'S', 'O', 'V', 'P', 'Z', 'J', 'A', 'Y', 'Q', 'U', 'I', 'R', 'H', 'X', 'L', 'N', 'F', 'T', 'G', 'K', 'D', 'C', 'M', 'W', 'B'], # Turnover: J
-    ['V', 'Z', 'B', 'R', 'G', 'I', 'T', 'Y', 'U', 'P', 'S', 'D', 'N', 'H', 'L', 'X', 'A', 'W', 'M', 'J', 'Q', 'O', 'F', 'E', 'C', 'K']  # Turnover: Z
+    {
+        "rotor": ['E', 'K', 'M', 'F', 'L', 'G', 'D', 'Q', 'V', 'Z', 'N', 'T', 'O', 'W', 'Y', 'H', 'X', 'U', 'S', 'P', 'A', 'I', 'B', 'R', 'C', 'J'],
+        "turnover": "Q"
+    },
+    {
+        "rotor": ['A', 'J', 'D', 'K', 'S', 'I', 'R', 'U', 'X', 'B', 'L', 'H', 'W', 'T', 'M', 'C', 'Q', 'G', 'Z', 'N', 'P', 'Y', 'F', 'V', 'O', 'E'],
+        "turnover": "E"
+    },
+    {
+        "rotor": ['B', 'D', 'F', 'H', 'J', 'L', 'C', 'P', 'R', 'T', 'X', 'V', 'Z', 'N', 'Y', 'E', 'I', 'W', 'G', 'A', 'K', 'M', 'U', 'S', 'Q', 'O'],
+        "turnover": "V"
+    },
+    {
+        "rotor": ['E', 'S', 'O', 'V', 'P', 'Z', 'J', 'A', 'Y', 'Q', 'U', 'I', 'R', 'H', 'X', 'L', 'N', 'F', 'T', 'G', 'K', 'D', 'C', 'M', 'W', 'B'],
+        "turnover": "J"
+    },
+    {
+        "rotor": ['V', 'Z', 'B', 'R', 'G', 'I', 'T', 'Y', 'U', 'P', 'S', 'D', 'N', 'H', 'L', 'X', 'A', 'W', 'M', 'J', 'Q', 'O', 'F', 'E', 'C', 'K'],
+        "turnover": "Z"
+    }
 ]
-
-def rotor_selection():
-    """Choose three rotors"""
-
-    rotors = []
-    for i in range(0, 3):
-        rotor_i = input(f"Rotor {i+1}: ")
-        rotors.append(rotor_i)
-
-
-    return rotors
-
 
 
 def initialise_rotor_position(rotor, position):
     """Bring rotor into the desired starting position"""
 
-    rotor = all_rotors[rotor-1]
+    rotor = all_rotors[rotor]
     starting_letter_index = rotor.index(position)
     
     # Split list at starting letter, then append new list end (wrapping around)
@@ -38,49 +57,69 @@ def initialise_rotor_position(rotor, position):
     return new_rotor
 
 
+
 def initialise_rotor_ring():
     # TODO
     """Bring rotor ring position into desired state"""
     ...
-    # ?
+    #>> ?
 
 
-def rotate_rotor(rotor):
+
+def rotate_rotor():
     # TODO
     """Rotate the rotors (change order of letters in list)"""
-    # Rotate rotor 3 --> if rotor 3 is at its turnover position --> rotate rotor 2 --> if rotor 2 is at its turnover position --> rotate rotor 1
-    ...
+
+    # If rotor 3 is not at its turnover position
+    if rotor_3["rotor"][0] != rotor_3["turnover"]:
+        #>> rotate rotor 3
+        ...
+    # If rotor 3 is at its turnover position
+    elif rotor_3["rotor"][0] == rotor_3["turnover"]:
+        #>> rotate rotor 3
+        ...
+        # If rotor 2 is at its turnover position
+        if rotor_2["rotor"][0] == rotor_2["turnover"]:
+            #>> rotate rotor 1
+            ...
+        #>> rotate rotor 2
+        ...
+
+    return
 
 
-
-
-def encrypt(rotors_selected, starting_letter):
-    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
-    rotor_1 = all_rotors[rotors_selected[0]-1]
-    rotor_2 = all_rotors[rotors_selected[1]-1]
-    rotor_3 = all_rotors[rotors_selected[2]-1]
-
+def encrypt(starting_letter):
+    """Encrypt input letters through same method as original Enigma (without plugboard)"""
+    
     # Get the letter on each rotor at the alphabet's index of the previous letter
-    rotor_3_letter = rotor_3[alphabet.index(starting_letter)]
+    rotor_3_letter = rotor_3["rotor"][alphabet.index(starting_letter)]
     # print(rotor_3_letter)
-    rotor_2_letter = rotor_2[alphabet.index(rotor_3_letter)]
+    rotor_2_letter = rotor_2["rotor"][alphabet.index(rotor_3_letter)]
     # print(rotor_2_letter)
-    rotor_1_letter = rotor_1[alphabet.index(rotor_2_letter)]
+    rotor_1_letter = rotor_1["rotor"][alphabet.index(rotor_2_letter)]
     # print(rotor_1_letter)
     reflector_letter = reflector_b[alphabet.index(rotor_1_letter)]
     # print(reflector_letter)
-    rotor_1_reversed_letter = alphabet[rotor_1.index(reflector_letter)]
+    rotor_1_reversed_letter = alphabet[rotor_1["rotor"].index(reflector_letter)]
     # print(rotor_1_reversed_letter)
-    rotor_2_reversed_letter = alphabet[rotor_2.index(rotor_1_reversed_letter)]
+    rotor_2_reversed_letter = alphabet[rotor_2["rotor"].index(rotor_1_reversed_letter)]
     # print(rotor_2_reversed_letter)
-    final_letter = alphabet[rotor_3.index(rotor_2_reversed_letter)]
+    final_letter = alphabet[rotor_3["rotor"].index(rotor_2_reversed_letter)]
     print(final_letter)
 
+
+
+def main():
+    global rotor_1
+    global rotor_2
+    global rotor_3
+
+    # Initialise rotors to be used
+    rotor_1 = all_rotors[0]
+    rotor_2 = all_rotors[1]
+    rotor_3 = all_rotors[2]
     
+    encrypt("A")
 
 
-
-
-# encrypt([1, 2, 3], input("Starting letter: ").upper())
-# initialise_rotor_position(5, "Ws")
+main()
