00245a9e2beed92f7f4ad466998d5bfa119a02c0
gregolator <gweiser54@icloud.com>
2025-02-04 19:14:53 +0100

    Minor changes

--- new_enigma.py
+++ new_enigma.py
@@ -3,21 +3,6 @@ alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N'
 reflector = ['E', 'J', 'M', 'Z', 'A', 'L', 'Y', 'X', 'V', 'B', 'W', 'F', 'C', 'R', 'Q', 'U', 'O', 'N', 'T', 'S', 'P', 'I', 'K', 'H', 'G', 'D']
 reflector_b = ['Y', 'R', 'U', 'H', 'Q', 'S', 'L', 'D', 'P', 'X', 'N', 'G', 'O', 'K', 'M', 'I', 'E', 'B', 'F', 'Z', 'C', 'W', 'V', 'J', 'A', 'T']
 
-# all_rotors = [
-#     ['E', 'K', 'M', 'F', 'L', 'G', 'D', 'Q', 'V', 'Z', 'N', 'T', 'O', 'W', 'Y', 'H', 'X', 'U', 'S', 'P', 'A', 'I', 'B', 'R', 'C', 'J'], # Turnover: Q
-#     ['A', 'J', 'D', 'K', 'S', 'I', 'R', 'U', 'X', 'B', 'L', 'H', 'W', 'T', 'M', 'C', 'Q', 'G', 'Z', 'N', 'P', 'Y', 'F', 'V', 'O', 'E'], # Turnover: E
-#     ['B', 'D', 'F', 'H', 'J', 'L', 'C', 'P', 'R', 'T', 'X', 'V', 'Z', 'N', 'Y', 'E', 'I', 'W', 'G', 'A', 'K', 'M', 'U', 'S', 'Q', 'O'], # Turnover: V
-#     ['E', 'S', 'O', 'V', 'P', 'Z', 'J', 'A', 'Y', 'Q', 'U', 'I', 'R', 'H', 'X', 'L', 'N', 'F', 'T', 'G', 'K', 'D', 'C', 'M', 'W', 'B'], # Turnover: J
-#     ['V', 'Z', 'B', 'R', 'G', 'I', 'T', 'Y', 'U', 'P', 'S', 'D', 'N', 'H', 'L', 'X', 'A', 'W', 'M', 'J', 'Q', 'O', 'F', 'E', 'C', 'K']  # Turnover: Z
-# ]
-
-# all_rotors = {
-#     "rotor_one": ['E', 'K', 'M', 'F', 'L', 'G', 'D', 'Q', 'V', 'Z', 'N', 'T', 'O', 'W', 'Y', 'H', 'X', 'U', 'S', 'P', 'A', 'I', 'B', 'R', 'C', 'J'],
-#     "rotor_two": ['A', 'J', 'D', 'K', 'S', 'I', 'R', 'U', 'X', 'B', 'L', 'H', 'W', 'T', 'M', 'C', 'Q', 'G', 'Z', 'N', 'P', 'Y', 'F', 'V', 'O', 'E'],
-#     "rotor_three": ['B', 'D', 'F', 'H', 'J', 'L', 'C', 'P', 'R', 'T', 'X', 'V', 'Z', 'N', 'Y', 'E', 'I', 'W', 'G', 'A', 'K', 'M', 'U', 'S', 'Q', 'O'],
-#     "rotor_four": ['E', 'S', 'O', 'V', 'P', 'Z', 'J', 'A', 'Y', 'Q', 'U', 'I', 'R', 'H', 'X', 'L', 'N', 'F', 'T', 'G', 'K', 'D', 'C', 'M', 'W', 'B'],
-#     "rotor_five": ['V', 'Z', 'B', 'R', 'G', 'I', 'T', 'Y', 'U', 'P', 'S', 'D', 'N', 'H', 'L', 'X', 'A', 'W', 'M', 'J', 'Q', 'O', 'F', 'E', 'C', 'K']
-# }
 
 all_rotors = [
     {
@@ -53,7 +38,6 @@ def initialise_rotor_position(rotor, position):
     return 
 
 
-
 def initialise_rotor_ring():
     # TODO
     """Bring rotor ring position into desired state"""
@@ -63,7 +47,7 @@ def initialise_rotor_ring():
 
 
 def rotate_rotors():
-    """Rotate the rotors (change order of letters in list)"""
+    """Rotate the rotors (change order of letters in list) after each keypress"""
 
     # If rotor 3 is not at its turnover position
     if rotor_3["rotor"][0] != rotor_3["turnover"]:
@@ -131,10 +115,13 @@ def main():
     initialise_rotor_position(rotor_2, rotor_2_starter)
     initialise_rotor_position(rotor_3, rotor_3_starter)
 
+    # TODO: DIFFERENT ENCRYPTION RESULT FOR DIFFERENT STARTER POSITIONS
+
 
     # Main encryption loop, runs as long as power on
     while True:
         # Get letter to encrypt
+        print(rotor_1["rotor"])
         letter = alphabet.index(input("Letter to encrypt: ")) + 1
         # Encrypt letter (and print it out)
         encrypt(letter)
