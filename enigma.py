d71158b7449377115422915176c772366836a53e
gregolator <gweiser54@icloud.com>
2025-02-10 20:44:29 +0100

    Added ring setting

--- enigma.py
+++ enigma.py
@@ -1,4 +1,4 @@
-"""Enigma I Emulation (Without Plugboard)"""
+"""Enigma I (no plugboard)"""
 
 # Alphabet reference
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -35,15 +35,18 @@ def initialise_rotor_position(rotor, position):
     shift = position - 1  # Convert to zero-based index
     rotor["rotor"] = rotor["rotor"][shift:] + rotor["rotor"][:shift] 
 
-
-def initialise_ring_setting(rotor):
+def initialise_ring_setting(rotor, ring_setting):
     """Initialise rotor's ring setting"""
-    # TODO
-    ...
+    # Shifts the rotor by the amount of offset required, same effect as offsetting the alphabet (relative difference is important)
+
+    shift = ring_setting - 1  # Convert to zero-based index
+    # Offset rotor
+    rotor["rotor"] = rotor["rotor"][shift:] + rotor["rotor"][:shift]
+    # Change turnover letter by offsetting it the same amount as ring setting
+    rotor["turnover"] = alphabet[(alphabet.index(rotor["turnover"]) - shift) % 26]  
 
 def rotate_rotors():
     """Simulate rotor stepping mechanism"""
-
     global rotor_1, rotor_2, rotor_3
 
     # Step rotor 3 with every keypress
@@ -57,10 +60,8 @@ def rotate_rotors():
         if rotor_2["rotor"][0] == rotor_2["turnover"]:
             rotor_1["rotor"] = rotor_1["rotor"][1:] + rotor_1["rotor"][:1]
 
-
 def encrypt(letter):
     """Encrypt a single letter"""
-
     index = alphabet.index(letter)  # Convert letter to index
 
     # Forward through rotors
@@ -81,7 +82,6 @@ def encrypt(letter):
 
     return letter
 
-
 # Main function
 def main():
     global rotor_1, rotor_2, rotor_3
@@ -96,10 +96,21 @@ def main():
     rotor_2_starter = int(input("Rotor 2 starter position: "))
     rotor_3_starter = int(input("Rotor 3 starter position: "))
 
+    # Get ring settings
+    rotor_1_ring = int(input("Rotor 1 ring setting: "))
+    rotor_2_ring = int(input("Rotor 2 ring setting: "))
+    rotor_3_ring = int(input("Rotor 3 ring setting: "))
+
     # Set rotors to correct positions
     initialise_rotor_position(rotor_1, rotor_1_starter)
     initialise_rotor_position(rotor_2, rotor_2_starter)
     initialise_rotor_position(rotor_3, rotor_3_starter)
+
+
+    # Set ring settings
+    initialise_ring_setting(rotor_1, rotor_1_ring)
+    initialise_ring_setting(rotor_2, rotor_2_ring)
+    initialise_ring_setting(rotor_3, rotor_3_ring)
 
     # Main encryption loop
     while True:
